95a7faa9999ddcdbe6eb0bdbd312e8ae18d29df3
wxj <kyfb@filifun.com>
2024-01-14 16:55:41 +0800

    rm

--- README.md
+++ README.md
@@ -3,29 +3,15 @@
 ```
 run: 
     python run.py
-
 ```
 
 #### 项目介绍与注意事项： 
-本项目用于合成数字人短视频。
+本项目用于合成三方数字人短视频，可用于heygen的部分平替（因为hg的审核越来越严格）。
 输入源视频+音频，输出口型匹配的高清视频。
 
+#### 实现流程原理
+运用Wav2lip将音频与视频进行口型同步合成，再运用GFP-GAN进行超分提升画质
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#### 联系我们
+微信/抖音：Filifun
+WaytoAIGC通往AIGC之路知识库：https://waytoaigc.feishu.cn/wiki/OHPuwkdwgia8eWkhvuHcORiEnRc
--- run copy.py
+++ run copy.py
@@ -3,30 +3,31 @@ import random
 import cv2
 from tqdm import tqdm
 from os import path
+import asyncio
 
-def run_command(cmd):
+async def run_command(cmd):
     print(cmd)
     return_code = os.system(cmd)
     return return_code
 
 # 用于使用wav2lip处理视频
-def process_video_with_wav2lip(input_video_path, input_audio_path, output_path):
+async def process_video_with_wav2lip(input_video_path, input_audio_path, output_path):
     wav2lip_cmd = f"/root/miniconda3/bin/python {wav2lipFolderName}/inference.py --checkpoint_path {wav2lipFolderName}/checkpoints/wav2lip.pth --face {input_video_path} --audio {input_audio_path} --outfile {output_path} --resize_factor 2 --fps 60  --face_det_batch_size 8 --wav2lip_batch_size 128"
-    result = run_command(wav2lip_cmd)
+    result = await run_command(wav2lip_cmd)
     if result != 0:
         print("An error occurred while running Wav2Lip")
     return result
 
 # 用于使用gfpgan处理图像 
-def process_images_with_gfpgan(input_folder, output_folder):  # 修改路径
+async def process_images_with_gfpgan(input_folder, output_folder):  # 修改路径
     gfpgan_cmd = f"/root/miniconda3/bin/python {gfpganPath}/inference_gfpgan.py -i {input_folder} -o {output_folder} -v 1.4 -s 2 --only_center_face --bg_upsampler None"
-    result = run_command(gfpgan_cmd)
+    result = await run_command(gfpgan_cmd)
     if result != 0:
         print("An error occurred while running GFPGAN")
     return result       
 
 # 用于从视频中提取帧
-def extract_frames_from_video(video_path, output_folder):
+async def extract_frames_from_video(video_path, output_folder):
     vidcap = cv2.VideoCapture(video_path)
     number_of_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = vidcap.get(cv2.CAP_PROP_FPS)
@@ -48,7 +49,7 @@ wav2lipPath = os.path.join(basePath, wav2lipFolderName)
 gfpganPath = os.path.join(basePath, gfpganFolderName)
 
 # 用于从提取的帧创建视频
-def create_videos_from_frames(frames_folder, output_folder, fps, batch_size):
+async def create_videos_from_frames(frames_folder, output_folder, fps, batch_size):
     dir_list = os.listdir(frames_folder)
     dir_list.sort()
 
@@ -96,7 +97,7 @@ def create_videos_from_frames(frames_folder, output_folder, fps, batch_size):
     return batch  # 返回批次数
 
 # 用于将多个视频文件合成一个视频文件
-def concatenate_videos(output_folder, batch_count, concat_filepath, output_filepath):
+async def concatenate_videos(output_folder, batch_count, concat_filepath, output_filepath):
     with open(concat_filepath, "w", encoding='utf8') as concat_text_file:
         for ips in range(batch_count):
             concat_text_file.write(f"file batch_{str(ips).zfill(4)}.mp4\n")
@@ -105,19 +106,18 @@ def concatenate_videos(output_folder, batch_count, concat_filepath, output_filep
     os.system(cmd)
 
 # 用于将音频和视频合并
-def merge_audio_and_video(video_path, audio_path, output_path):
+async def merge_audio_and_video(video_path, audio_path, output_path):
     cmd = f"ffmpeg -y -i {video_path} -i {audio_path} -map 0 -map 1:a -c:v copy -shortest {output_path}"
     os.system(cmd)
 
 userAudioPathList = os.listdir(os.path.join("inputs", "source_audio"))
 userVideoPathList = os.listdir(os.path.join("inputs", "source_video"))
-for sourceAudioName in userAudioPathList:
-    title = sourceAudioName.split(".")[-2]
-   # userVideoPathList = os.listdir(os.path.join("inputs", userPath, "source_video"))
-   # sourceVideoName = random.sample(userVideoPathList, 1)[0]
-    sourceVideoName = f"{title}.mp4"
 
+async def process_audio_video_async(sourceAudioName, basePath, userVideoPathList):
+    title = sourceAudioName.split(".")[-2]
+    sourceVideoName = f"{title}.mp4"
     outputPath = os.path.join(basePath, "outputs", title)
+    
     if not os.path.exists(outputPath):
         os.makedirs(outputPath)
 
@@ -125,25 +125,35 @@ for sourceAudioName in userAudioPathList:
     inputVideoPath = os.path.join(basePath, "inputs", "source_video", sourceVideoName)
     lipSyncedOutputPath = os.path.join(basePath, 'outputs', title, "result.mp4")
 
-    process_video_with_wav2lip(inputVideoPath, inputAudioPath, lipSyncedOutputPath)
+    await process_video_with_wav2lip(inputVideoPath, inputAudioPath, lipSyncedOutputPath)
 
     unProcessedFramesFolderPath = os.path.join(outputPath, 'frames')
     if not os.path.exists(unProcessedFramesFolderPath):
         os.makedirs(unProcessedFramesFolderPath)
 
-    fps = extract_frames_from_video(lipSyncedOutputPath, unProcessedFramesFolderPath)
+    fps = await extract_frames_from_video(lipSyncedOutputPath, unProcessedFramesFolderPath)
 
     restoredFramesPath = os.path.join(outputPath, 'restored_imgs')
     if not os.path.exists(restoredFramesPath):
         os.makedirs(restoredFramesPath)
 
-    process_images_with_gfpgan(unProcessedFramesFolderPath, restoredFramesPath)
+    await process_images_with_gfpgan(unProcessedFramesFolderPath, restoredFramesPath)
 
     processedVideoOutputPath = outputPath
-    batch = create_videos_from_frames(restoredFramesPath, processedVideoOutputPath, fps, batch_size=600)
+    batch = await create_videos_from_frames(restoredFramesPath, processedVideoOutputPath, fps, batch_size=600)
     concatTextFilePath = os.path.join(outputPath, "concat.txt")
     concatedVideoOutputPath = os.path.join(outputPath, "concated_output.mp4")
-    concatenate_videos(outputPath, batch, concatTextFilePath, concatedVideoOutputPath)
+    await concatenate_videos(outputPath, batch, concatTextFilePath, concatedVideoOutputPath)
 
     finalProcessedOutputVideo = os.path.join(processedVideoOutputPath, 'final_with_audio.mp4')
-    merge_audio_and_video(concatedVideoOutputPath, inputAudioPath, finalProcessedOutputVideo)
+    await merge_audio_and_video(concatedVideoOutputPath, inputAudioPath, finalProcessedOutputVideo)
+
+async def main():
+    tasks = []
+    for sourceAudioName in userAudioPathList:
+        task = asyncio.create_task(process_audio_video_async(sourceAudioName, basePath, userVideoPathList))
+        tasks.append(task)
+        print("task: " + str(sourceAudioName))
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
